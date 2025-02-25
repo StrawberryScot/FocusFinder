3979bb9507f667a4cddd9ed01efb42c6eec99a7d
Fliss <fliss_d@hotmail.co.uk>
2025-02-25 15:18:52 +0000

    updated address on spaces model

--- FocusFinderApp/Models/Space.cs
+++ FocusFinderApp/Models/Space.cs
@@ -10,7 +10,11 @@ public class Space {
     [Key]
     public int Id { get; set; }
     public string? Name { get; set; }
-    public address? Address { get; set; }
+    public string? HouseNumber { get; set; }
+    public string? Street { get; set; }
+    public string? City { get; set; }
+    public string? County { get; set; }
+    public string? Postcode { get; set; }
     public string? ImageURL { get; set; }
     public DateTime? CreatedAt { get; set; }
     }
