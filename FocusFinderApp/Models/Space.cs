6c963cf36741d2afba23089ca3ec43d5b89b8f5b
arthurbotto <arthurgsbotto@hotmail.com>
2025-02-25 15:36:43 +0000

    Vaidators for space model

--- FocusFinderApp/Models/Space.cs
+++ FocusFinderApp/Models/Space.cs
@@ -9,14 +9,26 @@ public class Space {
 
     [Key]
     public int Id { get; set; }
+
     public string? Name { get; set; }
+
     public string? HouseNumber { get; set; }
+
     public string? Street { get; set; }
+
     public string? City { get; set; }
+
     public string? County { get; set; }
+
+    [RegularExpression(@"^[A-Za-z0-9\s\-]+$", ErrorMessage = "Invalid postcode format")]
     public string? Postcode { get; set; }
+
     public string? ImageURL { get; set; }
-    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
+
+
+    public List<Review>? Reviews { get; set; }
     }
 }
 
